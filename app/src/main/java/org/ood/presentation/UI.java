c6f61bbbaad72ce37a70aadd654e0a185d2266c5
CatDruid <dagwowlirare@gmail.com>
2026-04-16 13:09:08 +0200

    Created feature/UI/UI branch

--- app/src/main/java/org/ood/presentation/UI.java
+++ app/src/main/java/org/ood/presentation/UI.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class UI {
     private Scanner scanner;
     private EnvironmentalUI environmentalUI;
-    private RecyclingUI recyclingUI;
     private UICRUDInterface<MaterialCRUDUI> materialCRUDUI;
     private UICRUDInterface<ProductCRUIDUI> ProductCRUDUI;
 }
