edb104b69e86d0fa01b6ebff961667c78675613d
David Charlier <david.charlier@gmx.de>
2026-04-17 12:33:31 +0200

    added skeleton methods

--- app/src/main/java/org/ood/application/EnvironmentalImpactService.java
+++ app/src/main/java/org/ood/application/EnvironmentalImpactService.java
@@ -8,4 +8,12 @@ public class EnvironmentalImpactService {
     private RegistryInterface<ProductRegistry> productRegistry;
     private ImpactCalculationStrategy weightedByLifespanStrategy;
     private ImpactCalculationStrategy simpleSumStrategy;
+
+    public float CalculateByWeighByLifespan(int id) {
+        return 0.0f;
+    }
+
+    public float CalculateBySimpleLifespan(int id) {
+        return 0.0f;
+    }
 }
