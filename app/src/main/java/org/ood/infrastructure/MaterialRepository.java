bb0df2b713c97940f0f03aa67cddc8c5989e9681
A. Pretti <aprettimarin@gmail.com>
2026-04-21 11:15:15 +0200

    Added working skeleton for Material (full UI still pending) Initialization of Material classes in main. Changes in CRUDService methods (turned some abstract, fixed Record-type bug) Changes in Service (Product, Material) to befit the model. Both now access Repository, enabling a working skeleton to be implemented with the UI.

--- app/src/main/java/org/ood/infrastructure/MaterialRepository.java
+++ app/src/main/java/org/ood/infrastructure/MaterialRepository.java
@@ -6,9 +6,19 @@ import org.ood.domain.RepositoryInterface;
 import java.util.List;
 
 public class MaterialRepository implements RepositoryInterface<MaterialEntity> {
-    public boolean Add(MaterialEntity materialEntity) {return false;}
-    public List<MaterialEntity> RetrieveAll() {return null;}
-    public MaterialEntity RetrieveByID(int id) {return null;}
-    public boolean Update(MaterialEntity materialEntity) {return false;}
-    public boolean Delete(int id) {return false;}
+    public boolean Add(MaterialEntity materialEntity) {
+        return false;
+    }
+    public List<MaterialEntity> RetrieveAll() {
+        return null;
+    }
+    public MaterialEntity RetrieveByID(int id) {
+        return null;
+    }
+    public boolean Update(MaterialEntity materialEntity) {
+        return false;
+    }
+    public boolean Delete(int id) {
+        return false;
+    }
 }
