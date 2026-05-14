61ef10dcfdc5096c8361703074a23d9a85e36dfe
Nerfier <david.charlier@gmx.de>
2026-05-14 14:31:44 +0200

    Added error handling for nullpointer exceptions

--- app/src/main/java/org/ood/domain/impactStrategy/AbstractImpactStrategy.java
+++ app/src/main/java/org/ood/domain/impactStrategy/AbstractImpactStrategy.java
@@ -6,6 +6,9 @@ import org.ood.domain.entities.ProductEntity;
 public abstract class AbstractImpactStrategy implements ImpactCalculationStrategy{
 
     protected float CalculateRawPCF(ProductEntity product) {
+        // Error handling, if product is null or if there are no materials in the product
+        if(product == null || product.getMaterial().isEmpty()) {return 0.0f;}
+
         float sum = 0.0f;
         for(MaterialEntity material : product.getMaterial()) {
             sum += material.GetMass() * material.GetEmissionFactor();
