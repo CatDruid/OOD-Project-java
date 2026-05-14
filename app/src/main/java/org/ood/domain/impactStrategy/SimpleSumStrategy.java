68796ece8297e41b0b149db756ef5108ecbf3461
Nerfier <david.charlier@gmx.de>
2026-05-14 14:32:16 +0200

    Cleaned up SimpleSumStrategy and added error handling to WeightedByLifespanStrategy

--- app/src/main/java/org/ood/domain/impactStrategy/SimpleSumStrategy.java
+++ app/src/main/java/org/ood/domain/impactStrategy/SimpleSumStrategy.java
@@ -3,15 +3,9 @@ package org.ood.domain.impactStrategy;
 import org.ood.domain.entities.MaterialEntity;
 import org.ood.domain.entities.ProductEntity;
 
-public class SimpleSumStrategy implements ImpactCalculationStrategy {
+public class SimpleSumStrategy extends AbstractImpactStrategy {
     @Override
     public float CalculateImpact(ProductEntity product) {
-        float sum = 0.0f;
-
-        for(MaterialEntity material : product.getMaterial()) {
-            sum += (material.GetMass() * material.GetEmissionFactor());
-        }
-
-        return sum;
+        return CalculateRawPCF(product);
     }
 }
--- app/src/main/java/org/ood/domain/impactStrategy/WeightedByLifespanStrategy.java
+++ app/src/main/java/org/ood/domain/impactStrategy/WeightedByLifespanStrategy.java
@@ -5,6 +5,8 @@ import org.ood.domain.entities.ProductEntity;
 public class WeightedByLifespanStrategy extends AbstractImpactStrategy{
     @Override
     public float CalculateImpact(ProductEntity product) {
+        // Error handling if product is null or lifespan <= 0
+        if(product == null || product.GetEstimatedLifeSpan() <= 0.0f) {return 0.0f;}
         return CalculateRawPCF(product) / product.GetEstimatedLifeSpan();
     }
 }
