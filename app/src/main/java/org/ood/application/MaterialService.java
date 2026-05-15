bd0f9785007873e895b78887d346c3c62e7d6419
A. Pretti <aprettimarin@gmail.com>
2026-05-15 16:52:58 +0200

    Added Material and Product Record tests to ensure that they're correctly being created from their source entities Fixed method capitalization in Material and Product Record ToRecord and FromRecord

--- app/src/main/java/org/ood/application/MaterialService.java
+++ app/src/main/java/org/ood/application/MaterialService.java
@@ -7,7 +7,6 @@ import org.ood.presentation.records.EntityRecords.MaterialRecord;
 
 import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class MaterialService extends CRUDServiceAbstract<MaterialEntity, MaterialRecord, MaterialCUDSuccessfully> {
     /**Dependency injections for initialization.
@@ -40,7 +39,7 @@ public class MaterialService extends CRUDServiceAbstract<MaterialEntity, Materia
     public List<MaterialRecord> RetrieveAll() {
         try {
             return this.materialRegistry.RetrieveAll().stream()
-                    .map(MaterialRecord::fromEntity)
+                    .map(MaterialRecord::FromEntity)
                     .sorted(Comparator.comparingInt(MaterialRecord::id))
                     .toList();
         } catch (Exception e) {
@@ -52,7 +51,7 @@ public class MaterialService extends CRUDServiceAbstract<MaterialEntity, Materia
     public MaterialRecord RetrieveByID(int id) {
         MaterialEntity entity = this.materialRegistry.RetrieveByID(id);
         if(entity != null)
-            return MaterialRecord.fromEntity(entity);
+            return MaterialRecord.FromEntity(entity);
         else
             return null;
     }
---
+++ app/src/test/java/org/ood/presentation/entityRecords/MaterialRecordTests.java
@@ -0,0 +1,97 @@
+package org.ood.presentation.entityRecords;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.ood.domain.RecyclingCategory;
+import org.ood.domain.entities.MaterialEntity;
+import org.ood.presentation.records.EntityRecords.MaterialRecord;
+
+import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MaterialRecordTests {
+    MaterialEntity entity;
+    MaterialRecord record;
+    //Initializes the Material Entity and Record for the tests
+    @BeforeEach
+    void InitializeBaseObject() throws Exception {
+        entity = new MaterialEntity(
+                1,
+                "Entity",
+                RecyclingCategory.Clear_Glass,
+                0.01f,
+                0.02f
+        );
+        record = new MaterialRecord(
+                2,
+                "Record",
+                RecyclingCategory.Coloured_Glass,
+                1.01f,
+                2.02f
+        );
+    }
+
+    @Test
+    @DisplayName("Creates from material entity to record.")
+    void CheckThatCreatesFromEntity(){
+        MaterialRecord.FromEntity(entity);
+    }
+    @Test
+    @DisplayName("Creates material record name is same as generating entity.")
+    void CheckThatRecordNameMatchesEntity() {
+        assertEquals(MaterialRecord.FromEntity(entity).name(), entity.GetName());
+    }
+    @Test
+    @DisplayName("Creates material record id is same as generating entity.")
+    void CheckThatRecordIdMatchesEntity() {
+        assertEquals(MaterialRecord.FromEntity(entity).id(), entity.GetID());
+    }
+    @Test
+    @DisplayName("Creates material record category is same as generating entity.")
+    void CheckThatRecordCategoryMatchesEntity() {
+        assertEquals(MaterialRecord.FromEntity(entity).category(), entity.GetRecyclingCategory());
+    }
+    @Test
+    @DisplayName("Creates material record mass is same as generating entity.")
+    void CheckThatRecordMassMatchesEntity() {
+        assertEquals(MaterialRecord.FromEntity(entity).mass(), entity.GetMass());
+    }
+    @Test
+    @DisplayName("Creates material record emission factor is same as generating entity.")
+    void CheckThatRecordEmissionMatchesEntity() {
+        assertEquals(MaterialRecord.FromEntity(entity).emissionFactor(), entity.GetEmissionFactor());
+    }
+    @Test
+    @DisplayName("Creates from material record to entity.")
+    void CheckThatCreatesToEntity() throws Exception {
+        record.ToEntity();
+    }
+    @Test
+    @DisplayName("Creates material entity name is same as generating record.")
+    void CheckThatEntityNameMatchesRecord() throws Exception {
+        assertEquals(record.ToEntity().GetName(), record.name());
+    }
+    @Test
+    @DisplayName("Creates material entity id is same as generating record.")
+    void CheckThatEntityIdMatchesRecord() throws Exception {
+        assertEquals(record.ToEntity().GetID(), record.id());
+    }
+    @Test
+    @DisplayName("Creates material entity category is same as generating record.")
+    void CheckThatEntityCategoryMatchesRecord() throws Exception {
+        assertEquals(record.ToEntity().GetRecyclingCategory(), record.category());
+    }
+    @Test
+    @DisplayName("Creates material entity mass is same as generating record.")
+    void CheckThatEntityMassMatchesRecord() throws Exception {
+        assertEquals(record.ToEntity().GetMass(), record.mass());
+    }
+    @Test
+    @DisplayName("Creates material entity emission factor is same as generating record.")
+    void CheckThatEntityEmissionMatchesRecord() throws Exception {
+        assertEquals(record.ToEntity().GetEmissionFactor(), record.emissionFactor());
+    }
+
+}
