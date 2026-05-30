2b5ec3311bc095bf68aef150a34986730bff035b
A. Pretti <aprettimarin@gmail.com>
2026-05-30 17:30:58 +0200

    Domain and Infrastructure documentation.

--- app/src/main/java/org/ood/infrastructure/repositories/JSONProductRepository.java
+++ app/src/main/java/org/ood/infrastructure/repositories/JSONProductRepository.java
@@ -1,9 +1,15 @@
 package org.ood.infrastructure.repositories;
-
 import org.ood.domain.entities.ProductEntity;
 
+/**
+ * Long-term storage for {@link ProductEntity}.
+ *
+ * @see AbstractJSONRepository
+ * @see ProductEntity
+ */
 public class JSONProductRepository extends AbstractJSONRepository<ProductEntity> {
 
+    /** {@inheritDoc} */
     public JSONProductRepository(String path) {
         super(path);
     }
